fe014a45417033a1973207a339af96ec00bacb15
Gaurav Bharat <gaurav.mendse@gmail.com>
2026-01-28 16:25:34 +0530

    installed class-validator pkg; added global Validation Pipe for all incoming requests; validation rules PENDING

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { MessagesModule } from './messages/messages.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
+  // Add validation for all incoming requests
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3333);
 }
 void bootstrap();
