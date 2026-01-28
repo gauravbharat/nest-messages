9c8aea7893881644138053ffa75dc3d2e24cbc8b
Gaurav Bharat <gaurav.mendse@gmail.com>
2026-01-28 17:38:43 +0530

    adding comments describing Validation Pipeline working for create message

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -8,7 +8,16 @@ export class MessagesController {
     console.log('Header', header);
   }
 
-  // Applied validation class to the request handler
+  /** Applied validation class to the request handler.
+   *
+   * Validation Pipe working explained:
+   * STEP 1. Here our server uses the class-transformer package to transform the request body i.e. a literal object
+   * into an instance of the CreateMessageDto DTO class i.e. constructor or a class object
+   *
+   * STEP 2. Uses class-validator pkg to validate the instance
+   *
+   * STEP 3. If there are validation errors, respond immediately, otherwise provide body to request handler
+   */
   @Post()
   createMessage(@Body() body: CreateMessageDto) {
     console.log('Body', body);
